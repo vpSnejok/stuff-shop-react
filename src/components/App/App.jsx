90dfc9c7e6f54b197f0ebaab7875c3835056638c
Vitaliy Snezhkin <vpsnejok@gmail.com>
2023-06-13 01:35:45 +0300

    add

---
+++ src/components/App/App.jsx
@@ -0,0 +1,29 @@
+import React, { useEffect } from "react";
+import { useDispatch } from "react-redux";
+
+import AppRoutes from "../Routes/Routes";
+import Header from "../Header/Header";
+import Footer from "../Footer/Footer";
+import Sidebar from "../Sidebar/Sidebar";
+import { getCategories } from "../../features/categories/categoriesSlice";
+
+const App = () => {
+  const dispatch = useDispatch();
+
+  useEffect(() => {
+    dispatch(getCategories);
+  }, [dispatch]);
+
+  return (
+    <div className="app">
+      <Header />
+      <div className="container">
+        <Sidebar />
+        <AppRoutes />
+      </div>
+      <Footer />
+    </div>
+  );
+};
+
+export default App;
